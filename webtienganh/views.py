4e87145eece4b4883de0910847c712c8d9febf4a
KIENHELLO <161566462+KIENHELLO@users.noreply.github.com>
2025-07-13 10:09:43 +0700

    Update views.py

--- webtienganh/views.py
+++ webtienganh/views.py
@@ -1,16 +1,11 @@
-# webtienganh/views.py
-
 import os
 from django.views.generic import View
 from django.http import HttpResponse
-from django.conf import settings
 
 class FrontendAppView(View):
     def get(self, request):
         try:
-            # Đường dẫn tuyệt đối đến index.html
-            file_path = os.path.join(settings.BASE_DIR, 'front_end', 'build', 'index.html')
-            with open(file_path, 'r') as f:
+            with open(os.path.join('front_end', 'build', 'index.html')) as f:
                 return HttpResponse(f.read())
         except FileNotFoundError:
             return HttpResponse("index.html not found!", status=501)
